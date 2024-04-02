15223659a3fbf7ef5544946cf10d1eed2c04682a
gabriel <gabrielvalino25@hotmail.com>
2024-04-02 19:44:35 -0300

    add SEO optimizations

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,31 +4,40 @@ import { Container, Hero, Preview } from './styles'
 
 import previewImage from '../../assets/app-preview.png'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size="4xl" as="h1">
-          Agendamento descomplicado
-        </Heading>
-        <Text size="lg">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
+    <>
+      <NextSeo
+        title="Agendamento descomplicado | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre."
+      />
 
-        <ClaimUsernameForm />
-      </Hero>
+      <Container>
+        <Hero>
+          <Heading size="4xl" as="h1">
+            Agendamento descomplicado
+          </Heading>
+          <Text size="lg">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
 
-      <Preview>
-        <Image
-          src={previewImage}
-          alt="Calendário simbolizando aplicação em funcionamento"
-          height={400}
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+          <ClaimUsernameForm />
+        </Hero>
+
+        <Preview>
+          <Image
+            src={previewImage}
+            alt="Calendário simbolizando aplicação em funcionamento"
+            height={400}
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
